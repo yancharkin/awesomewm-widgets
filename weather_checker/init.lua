252d354110c77e0d4308f84c396c12035e7cc6bc
yancharkin <yancharkin@gmail.com>
2018-08-20 19:33:59 +0300

    Add initial version of weather_checker

---
+++ weather_checker/init.lua
@@ -0,0 +1,97 @@
+local os = os
+local io = io
+local wibox = require("wibox")
+local awful = require("awful")
+
+local ROOT = os.getenv("HOME").."/.config/awesome/weather_checker/"
+local CHECK_INTERVAL = 900
+local IMG_MARGIN = 6
+local FONT = "Source\ Code\ Pro Regular 16"
+local W_HOT = 29
+local W_COLD = -5
+local COLOR_HOT = "'#ffa560'"
+local COLOR_OK = "'#cdf3cd'"
+local COLOR_COLD = "'#65c4ff'"
+
+local icons_list = {
+        "clear-day.svg", "clear-night.svg", "cloudy.svg", "foggy.svg",
+        "partly-cloudy-day.svg", "partly-cloudy-night.svg", "rain.svg",
+        "sleet.svg", "snow.svg", "wind.svg"
+}
+
+local imagebox_weather = wibox.widget.imagebox()
+local container_weather = wibox.container.margin(imagebox_weather)
+local textbox_weather = wibox.widget.textbox()
+container_weather.margins = IMG_MARGIN
+textbox_weather:set_font(FONT)
+local weather_checker = wibox.widget {
+    container_weather,
+    textbox_weather,
+    layout = wibox.layout.fixed.horizontal
+}
+
+function recolor_icons(color)
+    for i, filename in ipairs(icons_list) do
+        filepath = ROOT.."/images/"..filename
+        new_content = {}
+        f = io.open(filepath, "r")
+        for line in io.lines(filepath) do
+            if string.find(line, "style=") ~= nil then
+                new_line = '     style="clip-rule:evenodd;fill-rule:evenodd;fill:'
+                        ..color..';fill-opacity:1" /></svg>'
+                table.insert(new_content, new_line)
+            else
+                table.insert(new_content, line)
+            end
+        end
+        f:close()
+        f = io.open(filepath, "w")
+        for i, v in ipairs(new_content) do
+            f:write(v.."\n")
+        end
+        f:close()
+    end
+end
+
+function update_weather_indicator(data)
+    temperature, icon_name = data:match("([^,]+),([^,]+)")
+    temperature_striped = temperature:gsub("˚", "")
+    temperature_number = tonumber(temperature_striped)
+    icon_name = icon_name:gsub("\n", "")
+    COLOR = COLOR_OK
+    if temperature_number >= W_HOT then
+        COLOR = COLOR_HOT
+    elseif temperature_number <= W_COLD then
+        COLOR = COLOR_COLD
+    end
+    recolor_icons(COLOR)
+    textbox_weather:set_markup("<span foreground="..COLOR..">"..temperature..'</span>')
+    imagebox_weather:set_image(ROOT.."images/"..icon_name)
+end
+
+function check_weather()
+    textbox_weather:set_text("?")
+    weather_checker = io.popen("python "..ROOT.."weather_checker.py", "r")
+    data = weather_checker:read()
+    weather_checker:close()
+    if data ~= "Error" then
+        update_weather_indicator(data)
+    end
+end
+
+awful.widget.watch(
+    "python "..ROOT.."weather_checker.py", CHECK_INTERVAL,
+    function(widget, stdout)
+        if stdout ~= "Error" then
+            update_weather_indicator(stdout)
+        end
+    end
+)
+
+weather_checker:connect_signal("button::press",
+    function()
+        check_weather()
+    end
+)
+
+return weather_checker
---
+++ weather_checker/weather_checker.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+# -*- coding: utf-8; -*-
+
+import sys
+from bs4 import BeautifulSoup
+import lxml
+
+def get_weather():
+    """Returns current temperature"""
+    with open(sys.path[0] + "/location", "r") as f:
+        URL = f.readlines()[0].strip()
+    try:
+        from urllib2 import Request as urllib_request
+        from urllib2 import urlopen as urllib_urlopen
+        from urllib2 import URLError as urllib_urlerror
+        from urllib2 import HTTPError as urllib_httperror
+    except:
+        from urllib.request import Request as urllib_request
+        from urllib.request import urlopen as urllib_urlopen
+        from urllib.request import URLError as urllib_urlerror
+        from urllib.request import HTTPError as urllib_httperror
+
+    req = urllib_request(URL)
+
+    try:
+        page = urllib_urlopen(req)
+        page_content = page.read()
+        soup = BeautifulSoup(page_content, "lxml")
+
+        temperature = soup.find_all("span", attrs={"class":"summary"})[0].get_text().split()[0]
+        icon_name = ""
+        for tag in soup.find_all("span", attrs={"class":"currently"}):
+            icon_name = tag.find_all("img")[0].get("alt").split()[0]
+
+        output = temperature + "," + icon_name + ".svg"
+        if sys.version_info.major == 2:
+            output = output.encode("utf-8")
+        print(output)
+        return temperature, icon_name
+
+    except urllib_urlerror as e:
+        print("Error")
+        #print(e.reason)
+        return e.reason
+    except urllib_httperror as e:
+        print("Error")
+        #print(e.code)
+        #print(e.read())
+        return e.code, e.read()
+    except:
+        print("Error")
+        return 1
+
+if __name__ == "__main__":
+    get_weather()
