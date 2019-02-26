719e4cf1d26b9c7fec2d1d81db1d640bce76790b
Ivan Yancharkin <yancharkin@gmail.com>
2019-02-26 12:03:40 +0400

    mail_checker - add timeout in mail_check.py

--- mail_checker/mail_checker.py
+++ mail_checker/mail_checker.py
@@ -4,6 +4,9 @@ import os
 import sys
 import subprocess
 import imaplib
+import signal
+
+class TimeoutException(Exception): pass
 
 ACCOUNTS_DIR = sys.path[0] + "/accounts/"
 PASSWD_DIR = ACCOUNTS_DIR + "passwords/"
@@ -27,17 +30,31 @@ def count_all_unread():
                 passwd = f.readline().strip()
         return passwd
 
-    for account in ACCOUNTS:
-        passwd = get_passwd(PASSWD_DIR + account[1])
-        mail = imaplib.IMAP4_SSL(account[2], account[3])
-        mail.login(account[0], passwd)
-        status, count = mail.status("INBOX", "(MESSAGES UNSEEN)")
-        count = int(count[0].split()[4][:-1])
-        if status == "OK":
-            all_unread += count
+    def signal_timeout(signum, frame):
+        """Handler for timeout signal"""
+        raise TimeoutException("Time out.")
 
-    print(all_unread)
-    return all_unread
+    signal.signal(signal.SIGALRM, signal_timeout)
+    signal.alarm(10) # Maximum execution time
+
+    try:
+        for account in ACCOUNTS:
+            passwd = get_passwd(PASSWD_DIR + account[1])
+            mail = imaplib.IMAP4_SSL(account[2], account[3])
+            mail.login(account[0], passwd)
+            status, count = mail.status("INBOX", "(MESSAGES UNSEEN)")
+            count = int(count[0].split()[4][:-1])
+            if status == "OK":
+                all_unread += count
+
+        print(all_unread)
+        return all_unread
+
+    except TimeoutException as e:
+        print(e)
+        return 0
+    except:
+        return 0
 
 if __name__ == "__main__":
     count_all_unread()
